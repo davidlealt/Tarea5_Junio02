50f4eaad23881a4237c93b5019573b9b13164ade
davidlealt <david.leal@diginteligencia.com>
2017-06-14 20:13:22 -0500

    Delete 5.3.js

--- 5.3.js
+++
@@ -1,8 +0,0 @@
-var shotImage = new Image();
-shotImage.src = 'shot.png';
-
-canvas.addEventListener ('click',
-                          function myFunction(click) {
-                            context.drawImage(shotImage, click.clientX-shotImage.width/2, click.clientY-shotImage.height/2)
-                          }
-                        );
